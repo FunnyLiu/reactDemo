1602dda89814f99efa8f8494fc0e4c25d0ae08fc
刘放 <362512489@qq.com>
2021-11-05 14:03:49 +0800

    feat: demo use effect

---
+++ useEffect/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from "react";
+import Effect from "./Effect.jsx";
+
+function App() {
+
+
+  return (
+    <div>
+      <Effect/>
+    </div>
+  );
+}
+
+export default App;
